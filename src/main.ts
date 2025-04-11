017f7b697964704eb88e24af8e53ee145c418743
Igor Izotikov PC <izotikov.igor1@yandex.ru>
2025-04-11 23:53:10 +0300

    Init

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import {createApp} from "vue";
+import App from './app/App.vue';
+import './app/ui/App.css';
+
+createApp(App).mount('#app');
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@entities': path.resolve(__dirname, 'src/entities'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+      '@widgets': path.resolve(__dirname, 'src/widgets')
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+})
